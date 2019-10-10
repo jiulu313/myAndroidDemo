0b6e211474cb866f97b2a97a290c4a1f51b8e8a4
zhanghongjun01 <zhanghongjun228@163.com>
2019-10-10 15:13:29 +0800

    xx

--- app/src/main/java/com/zhj/my/MainActivity.java
+++ app/src/main/java/com/zhj/my/MainActivity.java
@@ -1,7 +1,9 @@
 package com.zhj.my;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,16 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btnScroller).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this,ScrollerActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/zhj/my/view/MyButton.java
+++ app/src/main/java/com/zhj/my/view/MyButton.java
@@ -5,12 +5,17 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
+import android.view.View;
 import android.widget.Button;
 
 import com.zhj.my.utils.ViewUtil;
 
 @SuppressLint("AppCompatCustomView")
-public class MyButton extends Button {
+public class MyButton extends View {
+    int lastX;
+    int lastY;
+
+
     public MyButton(Context context) {
         super(context);
     }
@@ -23,15 +28,48 @@ public class MyButton extends Button {
         super(context, attrs, defStyleAttr);
     }
 
+
     @Override
     public boolean dispatchTouchEvent(MotionEvent event) {
-        Log.e("zh","MyButton -> dispatchTouchEvent event=" + ViewUtil.getEventMask(event.getAction()));
+        Log.e("zh", "MyButton -> dispatchTouchEvent event=" + ViewUtil.getEventMask(event.getAction()));
         return super.dispatchTouchEvent(event);
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        Log.e("zh","MyButton -> onTouchEvent event=" + ViewUtil.getEventMask(event.getAction()));
-        return super.onTouchEvent(event);
+        super.onTouchEvent(event);
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN: {
+                lastX = (int) event.getX();
+                lastY = (int) event.getY();
+                break;
+            }
+            case MotionEvent.ACTION_MOVE: {
+                int x = (int) getX();
+                int y = (int) getY();
+
+                int offsetX = x - lastX;
+                int offsetY = y - lastY;
+
+                Log.e("zh33","x=" + x + " y=" + y +  " lastX=" + lastX + "  lastY=" + lastY + "  offsetX=" + offsetX + "  offsetY=" + offsetY);
+
+                int left = getLeft() + offsetX;
+                int top = getTop() + offsetY;
+                int right = left + getWidth();
+                int bottom = top + getHeight();
+
+                Log.e("zh22","left=" + left + "  top=" + top + "  right=" + right + "  bottom=" + bottom);
+
+                layout(left, top, right, bottom);
+            }
+            break;
+            case MotionEvent.ACTION_UP:
+                break;
+        }
+
+
+        return true;
+
+
     }
 }
