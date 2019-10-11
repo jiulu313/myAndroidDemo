0f5426c3ba6e351b6983e9aa75ffd166fe28a77e
zhanghongjun01 <zhanghongjun228@163.com>
2019-10-11 16:35:02 +0800

    添加drawactivituy

--- app/src/main/java/com/zhj/my/MainActivity.java
+++ app/src/main/java/com/zhj/my/MainActivity.java
@@ -5,22 +5,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.btnScroller).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClass(MainActivity.this,ScrollerActivity.class);
+        findViewById(R.id.btnScroller).setOnClickListener(this);
+        findViewById(R.id.btnDraw).setOnClickListener(this);
+
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent();
+        switch (v.getId()) {
+            case R.id.btnScroller:
+                intent.setClass(MainActivity.this, ScrollerActivity.class);
                 startActivity(intent);
-            }
-        });
-
-
+                break;
+            case R.id.btnDraw:
+                intent.setClass(MainActivity.this, DrawActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/zhj/my/view/MyDrawView.java
@@ -0,0 +1,52 @@
+package com.zhj.my.view;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BitmapShader;
+import android.graphics.Camera;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Shader;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.zhj.my.R;
+
+public class MyDrawView extends View {
+    Paint paint = new Paint();
+
+    public MyDrawView(Context context) {
+        this(context,null);
+    }
+
+    public MyDrawView(Context context, AttributeSet attrs) {
+        this(context, attrs,0);
+    }
+
+    public MyDrawView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    private void init(Context context) {
+
+    }
+
+    Camera camera = new Camera();
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+//        canvas.drawColor(Color.parseColor("#ff2233"));
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+
+        canvas.save();
+        camera.rotateY(50); // 旋转 Camera 的三维空间
+        camera.applyToCanvas(canvas); // 把旋转投影到 Canvas
+        canvas.drawBitmap(bitmap, 0, 0, paint);
+        canvas.restore();
+
+
+    }
+}
